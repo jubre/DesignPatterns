6674194df3eee44ad0d72ee317bcb1fb4f677940
Junior Corman <junior.corman@gmail.com>
2012-03-21 02:49:40 -0500

    test DecoratorTest pass

--- src/test/java/patterns/structural/DecoratorTest.java
+++ src/test/java/patterns/structural/DecoratorTest.java
@@ -1,11 +1,18 @@
 package patterns.structural;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class DecoratorTest {
 	
 	private Componente componente;
+	
+	@Before
+	public void setUp(){
+		componente = new ComponenteImpl();
+	}
 
 	@Test
 	public void verifyBasicOperation(){
