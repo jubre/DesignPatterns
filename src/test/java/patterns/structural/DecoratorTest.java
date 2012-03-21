ad9e1fbc74a584d1af5280f6dae554d5f1923ecb
Junior Corman <junior.corman@gmail.com>
2012-03-21 02:43:04 -0500

    add support for Test, definition Test to Decorator

--- src/test/java/patterns/structural/DecoratorTest.java
+++ src/test/java/patterns/structural/DecoratorTest.java
@@ -1,5 +1,15 @@
 package patterns.structural;
 
-public class DecoratorTest {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
+public class DecoratorTest {
+	
+	private Componente componente;
+
+	@Test
+	public void verifyBasicOperation(){
+		String result = componente.basicOperation();
+		assertEquals("flujo Normal", result);
+	}
 }
